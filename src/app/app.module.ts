3d0ab1572356a8c79161157eb9e7d40eb37b1905
tanmoy-pal <122528983+tanmoy-pal@users.noreply.github.com>
2023-01-12 14:44:04 +0100

    Finished part 2 - Display a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
